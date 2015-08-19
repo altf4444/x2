da35ec118c25b218e78d7da71a5ee9a07ca9d854
altf4444 <a.neises@widag.de>
2015-08-19 15:44:09 +0200

    new files for 5.2

---
+++ de/x2Leads.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    
+'Edit Lead'=>'Lead bearbeiten',
+'View Lead'=>'Lead anzeigen',
+'Leads:'=>'Lead:',
+'{leads} List'=>'{leads} Liste',
+'Create {lead}'=>'{lead} erstellen',
+'Edit {lead}'=>'{lead} bearbeiten',
+'Delete {lead}'=>'{lead} löschen',
+'Convert to {contact}'=>'In {contact} umwandeln',
+'Convert to {opportunity}'=>'In {opportunity} umwandeln',
+'Import {leads}'=>'Import {leads}',
+'Export {leads}'=>'Export {leads}',
+);
